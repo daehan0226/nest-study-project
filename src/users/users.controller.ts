33c4896d2099ffadd846e882fa2981624b344e1f
daehan <dh16931@gmail.com>
2022-03-26 20:19:22 +0900

    add exception filter, logging middleware/interceptor, validation pipe

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,6 +12,9 @@ import {
     ClassSerializerInterceptor,
     UseInterceptors,
     UseGuards,
+    HttpException,
+    HttpStatus,
+    UseFilters,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -19,6 +22,9 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { VerifyEmailDto } from './dto/verify-email.dto';
 import { UserLoginDto } from './dto/user-login.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { HttpExceptionFilter } from 'src/filters/http-exception.filter';
+import { LoggingInterceptor } from 'src/interceptors/logging.interceptor';
+import { ValidationPipe } from 'src/validations.ts/validation.pipe';
 
 @Controller('users')
 export class UsersController {
@@ -36,17 +42,47 @@ export class UsersController {
         return;
     }
 
+    @Get('find')
+    @UseInterceptors(LoggingInterceptor)
+    @UseFilters(new HttpExceptionFilter())
+    async findtest() {
+        console.log('in router');
+        throw new HttpException(
+            {
+                status: HttpStatus.FORBIDDEN,
+                error: 'This is a custom message',
+            },
+            HttpStatus.FORBIDDEN,
+        );
+    }
+
     @Post('/login')
     async login(@Body() dto: UserLoginDto): Promise<string> {
         return;
     }
 
     @UseInterceptors(ClassSerializerInterceptor)
+    @UseInterceptors(LoggingInterceptor)
+    @UseFilters(new HttpExceptionFilter())
     @Get()
     findAll(
-        @Query('offset', new DefaultValuePipe(0), ParseIntPipe) offset: number,
-        @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+        @Query(
+            'offset',
+            new DefaultValuePipe(0),
+            new ValidationPipe(),
+            ParseIntPipe,
+        )
+        offset: number,
+        @Query(
+            'limit',
+            new DefaultValuePipe(10),
+            new ValidationPipe(),
+            ParseIntPipe,
+        )
+        limit: number,
     ) {
+        console.log('in router');
+        throw new HttpException('t', 400);
         return this.usersService.findAll();
     }
 
